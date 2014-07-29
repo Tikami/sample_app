b48c379603c543b0651376aa12505b3f74a40d93
Gowin Huang <gowin.huang@gmail.com>
2014-07-29 21:44:45 +0800

    Finished user modle

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -43,6 +43,14 @@ describe User do
     end
   end
 
+  describe "email have two continuous dot" do
+    it "should be invalid" do
+      @user.email = "foo@bar..com"
+      expect(@user).not_to be_valid
+    end
+  end
+
+
   describe "when email format is valid" do
     it "should be invalid" do
       addresses = %w[user@foo.COM A_Us-Er@f.b.org first.lst@foo.jp a+b@baz.cn]
@@ -67,6 +75,7 @@ describe User do
     it {should_not be_valid}
   end
 
+
   describe "when the password doesn't match the confirmaiton" do
     before {@user.password_confirmation = "mismatch"}
     it {should_not be_valid}
@@ -90,6 +99,14 @@ describe User do
       it {should_not eq user_for_invalid_password}
         specify {expect(user_for_invalid_password).to be_false}
     end
+  end
 
+  describe "change the email to downcase" do
+   # let(:mixed_case_email){"FoorBar@example.com"}
+    it "should be saved as all lower-case" do
+        @user.email = "FoorBar@example.com"
+        @user.save
+        expect(@user.email.downcase).to eq @user.reload.email
+      end
   end
 end
