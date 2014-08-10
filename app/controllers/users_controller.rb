3a913445975094a9b924ef10e4ab69f1900c56d9
Gowin Huang <gowin.huang@gmail.com>
2014-08-11 00:26:13 +0800

    Finish sign in and out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,10 +29,10 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        #sign_in @user
-        flash[:success] = "Welcome to the Sample App!"
+        sign_in @user
+        #flash[:success] = "Welcome to the Sample App!"
         #redirect_to @user
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to @user, notice: 'Welcome! User was successfully created.' }
         format.json { render action: 'show', status: :created, location: @user }
       else
         format.html { render action: 'new' }
